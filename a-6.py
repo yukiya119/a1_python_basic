8557e2c7f9f6049dcd572bdee2797ac0ca89cf2d
yukiya119 <yukinosuke911@gmail.com>
2022-01-27 13:39:04 +0900

    途中経過で保存

---
+++ a-6.py
@@ -0,0 +1,4 @@
+odd_numbers = [1, 3, 5, 7, 9]
+
+for number in range(0, 5):
+    print(odd_numbers[number])
---
+++ a-7.py
@@ -0,0 +1,4 @@
+even_numbers = [2, 4, 6, 8]
+
+for number in range(0, 4):
+    print(even_numbers[number] * 2)
